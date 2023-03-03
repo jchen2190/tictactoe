e8827f71a8d53688b9841b04ef0b8ad04a565098
Jon Chen <jchen2190@gmail.com>
2023-03-03 00:39:44 -0500

    update working game

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Project is created with Javascript.
 Go to [https://jchen2190@github.io/tictactoe](https://jchen2190@github.io/tictactoe)
 
 ## Insiration
+Learn this project based on a tutorial. The tutorial website is at [https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn](https://dev.to/bornasepic/pure-and-simple-tic-tac-toe-with-javascript-4pgn)
--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-const playerX = 'x'
-const playerO = 'circle'
+const statusDisplay = document.getElementById("gameStatus");
 
-const winCombination = [
+const winningCondition = [
     [0, 1, 2], // horizontal
     [3, 4, 5],
     [6, 7, 8],
@@ -12,6 +11,80 @@ const winCombination = [
     [2, 4, 6]
 ]
 
-const cellSlot = document.querySelectorAll("data-cell");
-const myBoard = document.getElementById('board');
-const myMsg = document.getElementById('message');
+let gameState = ["", "", "", "", "", "", "", "", ""];
+let gameActive = true;
+let currentPlayer = "X";
+
+const winningMessage = () => `Player ${currentPlayer} has won!`;
+const drawMessage = () => `Game has ended in a draw!`;
+const currentPlayerTurn = () => `It's ${currentPlayer}'s turn`;
+
+statusDisplay.innerHTML = currentPlayerTurn();
+
+
+function handleCellClick(clickedCellEvent) {
+    const clickedCell = clickedCellEvent.target;
+
+    const clickedCellIndex = parseInt(clickedCell.getAttribute('data-index')); // turn string to number after grabbing
+
+    // check if game has been played or paused
+    if (gameState[clickedCellIndex] !== "" || !gameActive) {
+        return;
+    }
+    
+    handleCellPlayed(clickedCell, clickedCellIndex);
+    handleResultValidation();
+}
+
+function handleCellPlayed(clickedCell, clickedCellIndex) {
+    gameState[clickedCellIndex] = currentPlayer;
+    clickedCell.innerHTML = currentPlayer;
+}
+
+function handleResultValidation() {
+    let roundWon = false;
+    for (let i = 0; i <= 7; i++) {
+        const winCondition = winningCondition[i];
+        let a = gameState[winCondition[0]];
+        let b = gameState[winCondition[1]];
+        let c = gameState[winCondition[2]];
+        if (a === '' || b === '' || c === '') {
+            continue;
+        }
+        if (a === b && b === c) {
+            roundWon = true;
+            break;
+        }
+    }
+    if (roundWon) {
+        statusDisplay.innerHTML = winningMessage();
+        gameActive = false;
+        return;
+    }
+
+    let roundDraw = !gameState.includes("");
+    if (roundDraw) {
+        statusDisplay.innerHTML = drawMessage();
+        gameActive = false;
+        return;
+    }
+
+    handlePlayerChange();
+}
+
+function handlePlayerChange() {
+    currentPlayer = currentPlayer === "X" ? "O" : "X";
+    statusDisplay.innerHTML = currentPlayerTurn();
+}
+
+function handleRestartGame() {
+    // reset everything
+    gameActive = true;
+    currentPlayer = "X";
+    gameState = ["", "", "", "", "", "", "", "", ""]
+    statusDisplay.innerHTML = currentPlayerTurn();
+    document.querySelectorAll('.cell').forEach(cell => cell.innerHTML = "");
+}
+
+document.querySelectorAll('.cell').forEach(cell => cell.addEventListener('click', handleCellClick));
+document.querySelector('#restartBtn').addEventListener('click', handleRestartGame);
